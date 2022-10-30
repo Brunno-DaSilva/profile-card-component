244ea74584369ac3d6f579b5f6c10cfafd02bf38
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-10-30 12:15:19 -0500

    adding initial sass styles and var

---
+++ src/components/CardMain/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./useCardMainStyles.scss";
+
+const CardMain: React.FC = () => {
+  return (
+    <div className="CardMain">
+      <h2>Main Card</h2>
+    </div>
+  );
+};
+
+export default CardMain;
