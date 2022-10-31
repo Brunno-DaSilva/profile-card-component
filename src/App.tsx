c36620122875a3d897f9e65fa6220eaae7cb5018
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-10-30 19:47:48 -0500

    added card and img

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import CardMain from "./components/CardMain";
+import CARDS_DATA from "./assets/CARD_DATA.js";
 import "./App.scss";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <CardMain />
+      {CARDS_DATA.map((card) => {
+        return (
+          <CardMain
+            key={card.id}
+            id={card.id}
+            name={card.name}
+            profilePicture={card.profilePicture}
+            age={card.age}
+            city={card.city}
+            followers={card.followers}
+            likes={card.likes}
+            uploadedPhotos={card.uploadedPhotos}
+          />
+        );
+      })}
     </div>
   );
 };
--- src/components/CardMain/index.tsx
+++ src/components/CardMain/index.tsx
@@ -1,10 +1,48 @@
 import React from "react";
+import { ReactComponent as ReactLogo } from "../../assets/images/bg-pattern-card.svg";
 import "./useCardMainStyles.scss";
 
-const CardMain: React.FC = () => {
+type CardDataProps = {
+  id: string;
+  name: string;
+  profilePicture: string;
+  age: string;
+  city: string;
+  followers: number;
+  likes: number;
+  uploadedPhotos: number;
+};
+const CardMain: React.FC<CardDataProps> = ({
+  id,
+  name,
+  profilePicture,
+  age,
+  city,
+  followers,
+  likes,
+  uploadedPhotos,
+}) => {
   return (
     <div className="CardMain">
-      <h2>Main Card</h2>
+      <div className="CardMain__top_bg">
+        <ReactLogo
+          style={{
+            borderTopRightRadius: "0.9rem",
+            borderTopLeftRadius: "0.9rem",
+          }}
+        />
+      </div>
+      <div className="CardMain__bottom">
+        <div className="CardMain__bottom-profile">
+          <div className="bottom-profile__img">
+            <img
+              src={require("../../assets/images/image-victor.jpg")}
+              alt={name}
+            />
+          </div>
+        </div>
+        <div className="CardMain__bottom-sm_info"></div>
+      </div>
     </div>
   );
 };
