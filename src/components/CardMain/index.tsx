64d9de3fb55a1e62b08fd41c5f2cbdada2a56710
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-11-01 08:50:48 -0500

    adding func to conver 1000 to 1k

--- src/components/CardMain/index.tsx
+++ src/components/CardMain/index.tsx
@@ -22,6 +22,30 @@ const CardMain: React.FC<CardDataProps> = ({
   likes,
   uploadedPhotos,
 }) => {
+  const intToString = (num: any) => {
+    num = num.toString().replace(/[^0-9.]/g, "");
+    if (num < 1000) {
+      return num;
+    }
+    let si = [
+      { v: 1e3, s: "K" },
+      { v: 1e6, s: "M" },
+      { v: 1e9, s: "B" },
+      { v: 1e12, s: "T" },
+      { v: 1e15, s: "P" },
+      { v: 1e18, s: "E" },
+    ];
+    let index;
+    for (index = si.length - 1; index > 0; index--) {
+      if (num >= si[index].v) {
+        break;
+      }
+    }
+    return (
+      (num / si[index].v).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") +
+      si[index].s
+    );
+  };
   return (
     <div className="CardMain">
       <div className="CardMain__top_bg">
@@ -51,7 +75,7 @@ const CardMain: React.FC<CardDataProps> = ({
         <div className="CardMain__bottom-sm_info">
           <div className="sm_info__container">
             <div className="sm_info__container-number">
-              <h4>{followers}</h4>
+              <h4>{intToString(followers)}</h4>
             </div>
             <div className="sm_info__container-title">
               <span>Followers</span>
@@ -59,7 +83,7 @@ const CardMain: React.FC<CardDataProps> = ({
           </div>
           <div className="sm_info__container">
             <div className="sm_info__container-number">
-              <h4>{likes}</h4>
+              <h4>{intToString(likes)}</h4>
             </div>
             <div className="sm_info__container-title">
               <span>Likes</span>
@@ -67,7 +91,7 @@ const CardMain: React.FC<CardDataProps> = ({
           </div>
           <div className="sm_info__container">
             <div className="sm_info__container-number">
-              <h4>{uploadedPhotos}</h4>
+              <h4>{intToString(uploadedPhotos)}</h4>
             </div>
             <div className="sm_info__container-title">
               <span>Photos</span>
